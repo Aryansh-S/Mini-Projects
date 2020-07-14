dc00cea1c9192510f717efe6ff29a06a485629b0
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-07-14 16:16:05 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <project-headers.h>
 
 str fin;  
+const bool compound = 1; 
+
+
 
 int main() {
   std::cin.tie(0)->sync_with_stdio(0); 
+  
+  fin += subject[
   
   std::cout << fin << "\n"; 
 }
