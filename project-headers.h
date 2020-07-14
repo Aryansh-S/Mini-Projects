bdaa2c7ba4b1a27f2056477dbdcf17f5df5a84e8
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-07-14 16:13:04 -0700

    Update project-headers.h

--- project-headers.h
+++ project-headers.h
@@ -1,3 +1,13 @@
 #pragma once
 
 #include <lib/truism.h>
+#include <random> 
+
+inline namespace rando {
+  template<class T> inline T rand(T l, T r) {
+    std::random_device device;
+    std::mt19937 generator(device());
+    std::uniform_int_distribution<T> distribution(l,r); 
+    return distribution(generator); 
+  }
+}
