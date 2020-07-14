6cd12f510c63c21fa1f5dfdb67242cddec234c00
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-07-14 16:19:01 -0700

    Update truism.h

--- lib/truism.h
+++ lib/truism.h
@@ -12,12 +12,12 @@ using std::pair;
 template<class T, int SZ> using ar = std::array<T,SZ>; 
 
 inline namespace v1 { 
-  const ar<pair<str,int>, 6> subject = { {"you", 1}, {"the universe", 2}, {"existence", 2}, {"god", 2}, {"the soul", 2}, {"subjects", 2}};
+  const ar<pair<str,int>, 6> subject = { {"you", 1}, {"the universe", 2}, {"existence", 2}, {"god", 2}, {"the soul", 2}, {"subjects", 2} };
     // 1 second person, 2 third person (subjects)
     
-  const ar<pair<str,str>, 4> verb = { {"embody", "embodies"}, {"create", "creates"}, {"emit", "emits"}, {"run", "runs"}};
+  const ar<pair<str,str>, 4> verb = { {"embody", "embodies"}, {"create", "creates"}, {"emit", "emits"}, {"run", "runs"} };
     // f second person, s third person (action verbs)
   
-  const ar<str, 7> object = {"everything", "nothing", "something", "the force within", "the universe", "ideologies", "objects"};
+  const ar<str, 7> object = { "everything", "nothing", "something", "the force within", "the universe", "ideologies", "objects" };
     // (objects)
 }
