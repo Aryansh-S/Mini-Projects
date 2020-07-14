e2615c392c14312cd1cfe8ede22a48ccd4e484a9
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-07-14 16:42:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 str fin;  
 const bool cmpnd = 1; 
 
-template<class T> inline size_t get(T var) { return rand(0, var.size() - 1); }
+template<class T> inline int get(T var) { return rand(0, (int)var.size() - 1); }
 
 inline str sent() {
   const auto sb = subject[get(subject)];
@@ -11,7 +11,7 @@ inline str sent() {
   const auto vb = verb[get(verb)]; 
   const str vb_str = sb.s == 1 ? vb.f : vb.s; 
   const str ob_str = object[get(object)]; 
-  return sb_str + vb_str + ob_str; 
+  return sb_str + " " + vb_str + " " + ob_str; 
 }
 
 int main() {
