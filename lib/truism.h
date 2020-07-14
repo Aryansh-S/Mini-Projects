72868fff4b045b69bce26af57cf11665eb74ae33
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-07-14 16:18:27 -0700

    Update truism.h

--- lib/truism.h
+++ lib/truism.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string> 
 #include <pair> 
+#include <array> 
 
 using str = std::string; 
 
@@ -8,13 +9,15 @@ using std::pair;
   #define f first
   #define s second
 
+template<class T, int SZ> using ar = std::array<T,SZ>; 
+
 inline namespace v1 { 
-  const pair<str,int> subject[] = { {"you", 1}, {"the universe", 2}, {"existence", 2}, {"god", 2}, {"the soul", 2}, {"subjects", 2}};
+  const ar<pair<str,int>, 6> subject = { {"you", 1}, {"the universe", 2}, {"existence", 2}, {"god", 2}, {"the soul", 2}, {"subjects", 2}};
     // 1 second person, 2 third person (subjects)
     
-  const pair<str,str> verb[] = { {"embody", "embodies"}, {"create", "creates"}, {"emit", "emits"}, {"run", "runs"}};
+  const ar<pair<str,str>, 4> verb = { {"embody", "embodies"}, {"create", "creates"}, {"emit", "emits"}, {"run", "runs"}};
     // f second person, s third person (action verbs)
   
-  const str object[] = {"everything", "nothing", "something", "the force within", "the universe", "ideologies", "objects"};
+  const ar<str, 7> object = {"everything", "nothing", "something", "the force within", "the universe", "ideologies", "objects"};
     // (objects)
 }
