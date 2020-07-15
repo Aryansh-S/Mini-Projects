c781af62584b484c68062bb6d1baa2f4046c2015
Aryansh Shrivastava <sendtoaryansh@gmail.com>
2020-07-15 15:40:43 -0700

    Update truism.h

--- lib/truism.h
+++ lib/truism.h
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string> 
 #include <utility>
-#include <array> 
+#include <vector>
 
 using str = std::string; 
 
@@ -10,15 +10,15 @@ using std::pair;
   #define s second
   #define mp std::make_pair
 
-template<class T, int SZ> using ar = std::array<T,SZ>; 
+using vec = std::vector; 
 
 inline namespace v1 { 
-  const ar<pair<str,int>, 6> subject = { mp("you", 1), mp("the universe", 2), mp("existence", 2), mp("god", 2), mp("the soul", 2), mp("subjects", 2) };
+  const vec<pair<str,int>> subject = { mp("you", 1), mp("the universe", 2), mp("existence", 2), mp("god", 2), mp("the soul", 2), mp("subjects", 2) };
     // 1 second person, 2 third person (subjects)
     
-  const ar<pair<str,str>, 4> verb = { mp("embody", "embodies"), mp("create", "creates"), mp("emit", "emits"), mp("run", "runs") };
+  const vec<pair<str,str>> verb = { mp("embody", "embodies"), mp("create", "creates"), mp("emit", "emits"), mp("run", "runs") };
     // f second person, s third person (action verbs)
   
-  const ar<str, 7> object = { "everything", "nothing", "something", "the force within", "the universe", "ideologies", "objects" };
+  const vec<str> object = { "everything", "nothing", "something", "the force within", "the universe", "ideologies", "objects" };
     // (objects)
 }
